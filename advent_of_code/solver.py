67ebe79753f7d9a1176598a39a96069d1840e656
Raphael <raphael.ribard@gmail.com>
2021-01-10 16:26:28 +0100

    Day 1 part 1 is complete
    a bit cleaner

--- advent_of_code/solver.py
+++ advent_of_code/solver.py
@@ -3,7 +3,7 @@ This module provides a Solver class which can load the data located in the Data 
 This Solver class will be able to solve the daily puzzles using class methods.
 """
 
-from daily_puzzles_solvers.day_1 import day_1_puzzle_solve
+from advent_of_code.daily_puzzles_solvers.day_1 import day_1_puzzle_solve
 
 class Solver():
     """
@@ -14,6 +14,7 @@ class Solver():
         self.data_path = data_path
         self.data = {}
         self.available_days = []
+        self.solutions = {}
 
     def __repr__(self):
         days = ''.join([4*" " + f"- Day {day}\n" for day in self.available_days])
@@ -39,8 +40,19 @@ class Solver():
         self.data = data_dict
         self.available_days = set(available_days)
 
+    def solve_day_1(self):
+        """
+        solve_day_1:    This function appends the day 1 result
+                        to the solutions class attribute (dictionary)
+        """
+        part_1_result = day_1_puzzle_solve(self.data['Day_1_part_1'])
+        self.solutions['day_1_part_1'] = part_1_result
+
+
 test = Solver()
 test.load_data()
+test.solve_day_1()
 print(test)
+print(test.solutions)
 # print(test.data['Day_1_part_1'])
 # print(test.data['Day_2_part_1'])
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='advent_of_code',
-    version='0.1.0',    
+    version='0.1.0',
     description='Advent of code Python Package',
     url='https://github.com/raphaelribard/Advent_Of_Code_2020',
     author='Raphael RIBARD',
     license='BSD 2-clause',
     packages=['advent_of_code'],
-    install_requires=['numpy',                     
+    install_requires=['numpy',
                       ],
 
     classifiers=[
